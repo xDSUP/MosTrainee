f6c6faecf2b2e63f0a1afc6c1120f1f665cb3496
xdsup <xD_HOHOHO>
2023-05-24 22:30:06 +0300

    migration - add h2 - fix test context - remove legacy

--- trainee-service/src/test/java/ru/btproject/traineeservice/TraineeServiceApplicationTests.java
+++ trainee-service/src/test/java/ru/btproject/traineeservice/TraineeServiceApplicationTests.java
@@ -2,8 +2,16 @@ package ru.btproject.traineeservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+        "spring.flyway.enabled=false", // тест миграций в соседнем тесте
+        "spring.datasource.driver-class-name=org.h2.Driver",
+        "spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_ON_EXIT=FALSE",
+        "spring.datasource.username=sa",
+        "spring.datasource.password=''"
+})
 class TraineeServiceApplicationTests {
 
     @Test
